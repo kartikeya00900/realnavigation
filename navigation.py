c3d9a8a43f48976035252dc365906b5ab8561d39
kartikeya00900 <75730256+kartikeya00900@users.noreply.github.com>
2023-05-06 22:38:59 +0530

    Add files via upload

---
+++ navigation.py
@@ -0,0 +1,37 @@
+import webbrowser as wb
+import speech_recognition as sr
+
+
+listener= sr.Recognizer()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+                  
+    except:
+        pass
+    return command
+
+def program():
+    command = take_command()
+
+    print(command)
+    
+    cmd=(command)
+
+    if cmd in command:
+        prefix=("http:/")
+        suffix=('.com')
+        wb.open(prefix+cmd+suffix)
+      
+
+
+    else:
+        program=='quit'
+
+while True:
+    program()
